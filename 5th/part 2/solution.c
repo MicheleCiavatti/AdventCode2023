30d11c7f1f706cbba11fe9ac0a4c3b008de9fa70
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-12-05 22:41:33 +0100

    Minor refactoring

--- 5th/part 2/solution.c
+++ 5th/part 2/solution.c
@@ -7,18 +7,11 @@
 #include "solution.h"
 #include <limits.h>
 
-
 #define INPUT "input.txt"
 #define MAX_RANGES 100
 #define STEP_AMOUNT 8
 #define SEED_INDEX 0
 #define SOIL_INDEX 1
-#define FERTILIZER_INDEX 2
-#define WATER_INDEX 3
-#define LIGHT_INDEX 4
-#define TEMPERATURE_INDEX 5
-#define HUMIDITY_INDEX 6
-#define LOCATION_INDEX 7
 #define LIMIT ULLONG_MAX
 
 char buf[10000];
@@ -88,6 +81,36 @@ bool isMatching(unsigned long long start, unsigned long long src, unsigned long 
     return (    (start >= src && start < src + range)
             ||  (start + r >= src && start + r < src + range));
 }
+
+/*---------------------------------------------------EXPLANATION------------------------------------------------------*/
+/*
+A layer of the mapping is defined with variable type range_t, which has arrays src[], dst[], range[] and length of 
+these arrays.
+We call a layer as a "step", and we read all the steps from the input file and memorize them in variable steps[].
+For each single step, we add a padding: intervals not explicited are added to the step, whose mapping is an identity
+function.
+For example, suppose that step[1] doesn't cover interval for sources 0 - 20: therefore, we add another interval in
+step[1] at the the position step[1].length, like this: 
+    step[1].src[step[1].length] = 0; step[1].dst[step[1].length] = 0; step[1].range[step[1].length++] = 20
+Let's consider a specific interval I:
+                                    I
+            |start|----------------------------------|start+range|
+We pass I in every step: since we added padding, every single sub-interval of I will have a match at some point.
+Let's also consider a specific interval of src[] (r is a range) called S.
+                                   S
+            |src|------------------------------------|src+r|
+If I and S do not intersecate, I simply goes to the next interval of src[].
+If I and S do intersecate (we find out with function isMatching()), then we calculate the subinterval that matches.
+Ex:
+            |start|-------------------------------------|start+range|
+                                |src|------------------------------------------|src+r|
+                                    |___________________|
+                                      MATCHING INTERVAL
+We apply the mapping given by the step to the matching interval, and the matching interval goes to the next step.
+The interval I continues to the next src[], until finished.
+At step 8, the last, an interval J arrives to the compute() function (the recursive function we've been using) and
+it only checks if it's Jstart is < than min: if so, then min = Jstart.
+*/
 
 void compute(unsigned long long start, unsigned long long range, const int index) {
     printf("START: %llu - RANGE: %llu\n", start, range);
